5a0f7dd4f378af6aeeab5b1a85bc0d358f28219f
Cameron Pulsford <cpulsfor@gmail.com>
2010-10-05 21:17:14 -0400

    Cleaned up.

--- linear.clj
+++ linear.clj
@@ -4,74 +4,84 @@
 ;;
 
 ;;
-;; helpers
+;; helpers and utils
 ;;
 
-(defn- det-2x2 [[[a b] [c d]]]
+(defn domap
+  ([f x]
+   (doseq [i x] (f i)))
+  ([f x & more]
+   (doseq [i (apply map list x more)]
+     (apply f i))))
+
+(defn- det-2x2
+  [[[a b] [c d]]]
   (- (* (int a) (int d))
      (* (int b) (int c))))
 
-(defn drop-at [n m]
+(defn drop-at
+  [n m]
   (concat (take (int n) m) (drop (inc (int n)) m)))
 
-(defn- dimensions [m]
+(defn- dimensions
+  [m]
   [(count m) (count (first m))])
 
-(defn- square? [m]
+(defn- square?
+  [m]
   (let [[r c] (dimensions m)]
     (== r c)))
 
-(defn- interleave-signs [x]
-  (let [l (count (first x))]
-    (loop [i 0 x x coll []]
-      (if (seq x)
-        (if (even? i)
-          (recur (inc i) (rest x) (conj coll (first x) (take l (cycle [1 -1]))))
-          (recur (inc i) (rest x) (conj coll (first x) (take l (cycle [-1 1])))))
-        coll))))
+(defn- nested-cycle
+  [& more]
+  (cycle (map cycle more)))
+
+(defn- interleave-signs
+  [m]
+  (let [l (count (first m))]
+    (interleave m (map #(take l %) (nested-cycle [1 -1] [-1 1])))))
+
+(defn- mat-map
+  ([f m]
+   (map #(map f %) m))
+  ([f m1 m2]
+   (map #(map f %1 %2) m1 m2)))
 
 ;
 ;
 
-(defn matrix
-  "user=> (matrix [[1 2 3] [4 5 6] [7 8 9]])
-   ((1 2 3) (4 5 6) (7 8 9)) 
-
-   user=> (matrix [1 2 3] [4 5 6] [7 8 9])
-   ((1 2 3) (4 5 6) (7 8 9))
-
-   user=> (matrix [[1 2 3] [4] [7 8 9]])
-   Error Illegally sized matrix."
-  ([m] (if (apply == (map count m))
-         (map seq m)
-         (throw (Error. "Illegal sized matrix."))))
-  ([m1 & more] (matrix (list* m1 more))))
-
-(defn print-mat [m]
+(defn print-mat
+  [m]
   (doseq [x m] (println x)))
 
-(defn transpose [m]
+(defn transpose
+  [m]
   (apply map list m))
 
-(defn add [m1 m2]
-  (map (partial map +) m1 m2))
+(defn add
+  [m1 m2]
+  (mat-map + m1 m2))
 
-(defn sub [m1 m2]
-  (map (partial map -) m1 m2))
+(defn sub
+  [m1 m2]
+  (mat-map - m1 m2))
 
-(defn mul [m1 m2]
+(defn mul
+  [m1 m2]
   (for [a m1]
     (for [b (transpose m2)]
       (reduce + (map * a b)))))
 
-(defn minors [m]
+(defn minors
+  [m]
   (let [r (range (count m))]
     (for [a r b r]
       (->> m
-           (drop-at a) ; drop the i'th row
-           (map #(drop-at b %)))))) ; drop the j'th column
+        (drop-at a) ; drop the i'th row
+        (map #(drop-at b %)))))) ; drop the j'th column
 
-(defn det [m]
+(defn det
+  [m]
   (if [(square? m)]
     ((fn self [m] (if (= (dimensions m) [2 2])
                     (det-2x2 m)
@@ -80,26 +90,30 @@
                                      (map self (minors m)))))) m)
     (throw (Error. "Can not find the determinant of a non-square matrix."))))
 
-(defn cofactors [m]
+(defn cofactors
+  [m]
   (let [[r] (dimensions m)]
     (for [x (->> (minors m) (map det) (partition r) interleave-signs (partition 2))]
       (apply map * x))))
 
-(defn adjoint [m]
+(defn adjoint
+  [m]
   (-> m cofactors transpose))
 
-(defn inverse [m]
+(defn inverse
+  [m]
   (let [adj (adjoint m)
         d (det m)]
-    (if (zero? d)
-      (throw (Error. (format "Non-invertible matrix.")))
-      (for [row adj] (for [col row] (* (/ 1 d) col))))))
+    (if (not (zero? d))
+      (mat-map #(* % (/ 1 d)) adj)
+      (throw (Error. (format "Non-invertible matrix."))))))
 
 ;;
 ;; tests
 ;;
 
-(defn mat-test [x]
+(defn mat-test
+  [x]
   (mul x (inverse x)))
 
 (mat-test [[1 2 3 4] [4 5 6 1] [7 8 -9 3] [1 1 1 1]])
